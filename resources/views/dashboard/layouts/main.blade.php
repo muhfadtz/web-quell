341562e6bf3819e1b3a0ad55575e13f14be472c9
muhfadtz <mhfadtz@gmail.com>
2024-03-05 23:11:35 +0700

    fix: big for page title

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -4,7 +4,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>revam | Dashboard</title>
+    <title>Quell | Dashboard</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
 
